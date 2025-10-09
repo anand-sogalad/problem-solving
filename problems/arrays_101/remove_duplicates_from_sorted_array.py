6c9e89c81cee577ab16393509ade36fa08fdc1a1
Anand Sogalad <anand.sogalad305@gmail.com>
2025-10-09 07:59:12 +0530

    Add in-place dedup for arrays and refactor max
    Signed-off-by: Anand Sogalad <anand.sogalad305@gmail.com>

--- problems/arrays_101/remove_duplicates_from_sorted_array.py
+++ problems/arrays_101/remove_duplicates_from_sorted_array.py
@@ -9,3 +9,15 @@ class Solution:
             if nums[i - 1] == nums[i]:
                 del nums[i]
         return len(nums)
+
+
+# with 0(1) and 0(n) time
+class Solution1:
+    def removeDuplicates(self, arr: list[int]) -> list[int]:
+        write_pointer = 1
+        for read_pointer in range(1, len(arr)):
+            if arr[read_pointer] != arr[write_pointer - 1]:
+                arr[write_pointer] = arr[read_pointer]
+                write_pointer += 1
+        del arr[write_pointer:]
+        return arr
--- problems/arrays_101/replace_greatest_element.py
+++ problems/arrays_101/replace_greatest_element.py
@@ -9,6 +9,5 @@ class Solution:
         for i in range(len(arr) - 1, -1, -1):
             current = arr[i]
             arr[i] = max_right
-            if current > max_right:
-                max_right = current
+            max_right = max(max_right, current)
         return arr
