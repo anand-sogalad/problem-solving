a65face99f6c64e203351fd456004439eb10817e
Anand Sogalad <anand.sogalad305@gmail.com>
2025-11-02 20:05:19 +0530

    Annotate seen as set[int]
    Signed-off-by: Anand Sogalad <anand.sogalad305@gmail.com>

--- src/leetcode/arrays_101/check_if_n_and_its_double_exists.py
+++ src/leetcode/arrays_101/check_if_n_and_its_double_exists.py
@@ -1,6 +1,6 @@
 class solution:
     def check_if_exists(self, nums: list[int]):
-        seen = set()
+        seen: set[int] = set()
         for i in nums:
             if i * 2 in seen or (i % 2 == 0 and i // 2 in seen):
                 return True
