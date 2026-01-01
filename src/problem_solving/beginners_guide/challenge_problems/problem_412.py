0c1e5a5029e22be930a67a6341b7cb61dc0d816e
Anand Sogalad <anand.sogalad305@gmail.com>
2026-01-01 16:11:27 +0530

    add fizz buzz solution with performance timer

---
+++ src/problem_solving/beginners_guide/challenge_problems/problem_412.py
@@ -0,0 +1,25 @@
+from problem_solving.beginners_guide.decorators.preformance import timer
+
+
+class Solution:
+    @timer
+    @staticmethod
+    def fizz_buzz(num: int) -> list[str]:
+        nums: list[str] = []
+        for i in range(1, num + 1):
+            if i % 3 == 0 and i % 5 == 0:
+                nums.append("FizzBuzz")
+            elif i % 3 == 0:
+                nums.append("Fizz")
+            elif i % 5 == 0:
+                nums.append("Buzz")
+            else:
+                nums.append(str(i))
+        return nums
+
+
+if __name__ == "__main__":
+    test_data: int = 15
+
+    result = Solution.fizz_buzz(test_data)
+    print(result)
