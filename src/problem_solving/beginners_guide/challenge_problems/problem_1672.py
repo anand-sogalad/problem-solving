1731273c14325d5b3424b615e0feacafd59e52d8
Anand Sogalad <anand.sogalad305@gmail.com>
2026-01-01 14:02:09 +0530

    add solution for richest customer wealth problem with performance timer

---
+++ src/problem_solving/beginners_guide/challenge_problems/problem_1672.py
@@ -0,0 +1,36 @@
+from problem_solving.beginners_guide.decorators.preformance import timer
+
+
+class Solution:
+    @timer
+    @staticmethod
+    def richest_customer_wealth(wealth: list[list[int]]) -> int:
+        max_wealth: int = 0
+        for individual in wealth:
+            individual_health: int = 0
+            for number in individual:
+                individual_health += number
+            max_wealth = max(max_wealth, individual_health)
+        return max_wealth
+
+    @timer
+    @staticmethod
+    def richest_customer_wealth_improved(wealth: list[list[int]]) -> int:
+        return max(sum(i) for i in wealth)
+
+    @timer
+    @staticmethod
+    def richest_customer_wealth_improved1(wealth: list[list[int]]) -> int:
+        return max(sum(i) for i in wealth)
+
+
+if __name__ == "__main__":
+    test_data = [[7, 1, 3], [2, 8, 7], [1, 9, 5]]
+    result = Solution.richest_customer_wealth(test_data)
+    print(f"result: {result}")
+
+    result = Solution.richest_customer_wealth_improved(test_data)
+    print(f"result: {result}")
+
+    result = Solution.richest_customer_wealth_improved1(test_data)
+    print(f"result: {result}")
